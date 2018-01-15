08585c6e39dc2b12e0cec31b2f23a7276c829cb7
khaiphanvan <khaipv.11@grad.uit.edu.vn>
2018-01-15 09:43:19 +0700

    commit

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,26 +3,54 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 
+
 def get_sentinel_user():
     return get_user_model().objects.get_or_create(username='deleted')[0]
 
+
+class PublishedManage(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManage, self).get_queryset()\
+                                            .filter(status='draft')
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset()\
+                          .filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOISE = (
-        ('draft','Draft'),
-        ('published','Published'),
+        ('draft', 'Draft'),
+        ('published', 'Published'),
     )
 
-    tilte = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=250,unique_for_date='publish')
-    author = models.ForeignKey(User, related_name='blog_post',on_delete=models.SET(get_sentinel_user))
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, related_name='blog_post', on_delete=models.SET(get_sentinel_user))
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10,choices=STATUS_CHOISE,default='draft')
+    status = models.CharField(max_length=10, choices=STATUS_CHOISE, default='draft')
+    objects = models.Manager()
+    #published = PublishedManager()
+    published = PublishedManage()
+
     class Meta:
         ordering = ('-publish',)
+
     def __str__(self):
-        return self.tilte
+        return self.title
+
+    # def get_absolute_url(self):
+        # return reversed('blog:post_detail',
+        #                 args=[self.publish.year,
+        #                       self.publish.strftime('%m'),
+        #                       self.publish.strftime('%d'),
+        #                       self.slug])
+        # return   (str(self.publish.year)+'/'+self.publish.strftime('%m')+'/'+ self.publish.strftime('%d')+'/'+ self.slug)
 
 # Create your models here.
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    #path(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
+
 ]
