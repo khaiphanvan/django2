cdfe32628a1db4dc48a1c5ecda5b6b8799943719
khaiphanvan <khaipv.11@grad.uit.edu.vn>
2018-01-16 08:46:58 +0700

    commit 3

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -53,4 +53,17 @@ class Post(models.Model):
                               self.slug])
         # return   (str(self.publish.year)+'/'+self.publish.strftime('%m')+'/'+ self.publish.strftime('%d')+'/'+ self.slug)
 
-# Create your models here.
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'Comment'
+        ordering = ('created',)
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
